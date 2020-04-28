7d091a003192e8779c44b2eae57e5b2e0e1a563e
Ayadi <mahdiayadi@kpmg.fr>
2020-04-28 10:39:47 +0200

    Adding main files of package

--- src/classes.ts
+++ src/classes.ts
@@ -1,18 +1,18 @@
-export interface HelmetDetectionClass {
+export interface MaskDetectionClass {
     name: string;
     id: number;
     displayName: string;
   }
   
-  export const CLASSES: {[key: string]: HelmetDetectionClass} = {
-    2: {
+  export const CLASSES: {[key: string]: MaskDetectionClass} = {
+    1: {
       name: 'person',
       id: 1,
       displayName: 'person',
     },
-    1: {
-      name: 'hat',
+    2: {
+      name: 'mask',
       id: 2,
-      displayName: 'person with helmet',
+      displayName: 'person with mask',
     }
   };
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export {version} from './version';
 import {CLASSES} from './classes';
 
 
-export interface DetectedHelmet {
+export interface DetectedMask {
   bbox: [number, number, number, number];  // [x, y, width, height]
   class: string;
   score: number;
@@ -18,12 +18,12 @@ export async function load(path : string) {
         `also include @tensorflow/tfjs on the page before using this model.`);
   }
 
-  const helmetDetection = new HelmetDetection(path);
-  await helmetDetection.load();
-  return helmetDetection;
+  const maskDetection = new MaskDetection(path);
+  await maskDetection.load();
+  return maskDetection;
 }
 
-export class HelmetDetection {
+export class MaskDetection {
   private modelPath: string;
   private model: tfconv.GraphModel;
 
@@ -49,7 +49,7 @@ export class HelmetDetection {
    */
   private async infer(
       img: tf.Tensor3D|ImageData|HTMLImageElement|HTMLCanvasElement|
-      HTMLVideoElement): Promise<DetectedHelmet[]> {
+      HTMLVideoElement): Promise<DetectedMask[]> {
     const batched = tf.tidy(() => {
       if (!(img instanceof tf.Tensor)) {
         img = tf.browser.fromPixels(img);
@@ -114,9 +114,9 @@ export class HelmetDetection {
 
 private buildDetectedObjects(
   width: number, height: number, boxes: Float32Array, scores: Float32Array,
-  indexes: Float32Array, classes: Float32Array): DetectedHelmet[] {
+  indexes: Float32Array, classes: Float32Array): DetectedMask[] {
     const count = indexes.length;
-    const objects: DetectedHelmet[] = [];
+    const objects: DetectedMask[] = [];
     for (let i = 0; i < count; i++) {
       const bbox = [];
       for (let j = 0; j < 4; j++) {
@@ -150,7 +150,7 @@ private buildDetectedObjects(
    */
   async detect(
       img: tf.Tensor3D|ImageData|HTMLImageElement|HTMLCanvasElement|
-      HTMLVideoElement): Promise<DetectedHelmet[]> {
+      HTMLVideoElement): Promise<DetectedMask[]> {
     return this.infer(img);
   }
 
